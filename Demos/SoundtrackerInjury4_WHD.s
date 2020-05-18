1af5cbcd284c05cac43e193096e557bbbe21e3a4
StingRay/Scarab^Scoopex <32897354+MK1Roxxor@users.noreply.github.com>
2020-05-18 18:37:00 +0200

    Add files via upload

---
+++ Demos/SoundtrackerInjury4_WHD.s
@@ -0,0 +1,261 @@
+***************************************************************************
+*             /                                                           *
+*       _____.__ _                                         .___.          *
+*      /    /_____________.  _________.__________.________ |   |________  *
+*  ___/____      /    ____|_/         |         /|        \|   ._      /  *
+*  \     \/      \    \     \    /    |    :___/¯|    \    \   |/     /   *
+*   \_____________\___/_____/___/_____|____|     |____|\_____________/    *
+*     -========================/===========|______\================-      *
+*                                                                         *
+*   .---.----(*( SOUNDTRACKER INJURY4/VISUAL ARTS WHDLOAD SLAVE)*)---.---.*
+*   `-./                                                              \.-'*
+*                                                                         *
+*                         (c)oded by StingRay                             *
+*                         --------------------                            *
+*                                May 2020                                 *
+*                                                                         *
+*                                                                         *
+***************************************************************************
+
+***********************************
+*** History			***
+***********************************
+
+; 16-May-2020	- work started
+;		- and finished a few minutes later as I could reused
+;		  a lot of the code I made for the first Soundtracker
+;		  Injury patch
+;		- 
+;		- DMA wait in replayer fixed, byte write to volume register
+;		  fixed, Bplcon0 color bit fixes (x2), keyboard routine fixed,
+;		  DMA settings fixed
+
+	INCDIR	SOURCES:INCLUDE/
+	INCLUDE	WHDLoad.i
+	
+
+FLAGS		= WHDLF_NoError|WHDLF_ClearMem|WHDLF_EmulTrap
+QUITKEY		= $59		; F10
+DEBUG
+
+; absolute skip
+PL_SA	MACRO
+	PL_S	\1,\2-(\1)
+	ENDM
+
+; jsr+absolute skip
+PL_PSA	MACRO
+	PL_PS	\1,\2		; could use PSS here but it fills memory
+	PL_S	\1+6,\3-(\1+6)	; with NOPS so we use standard skip
+	ENDM
+
+
+HEADER	SLAVE_HEADER		; ws_security + ws_ID
+	dc.w	10		; ws_version
+	dc.w	FLAGS		; flags
+	dc.l	524288		; ws_BaseMemSize
+	dc.l	0		; ws_ExecInstall
+	dc.w	Patch-HEADER	; ws_GameLoader
+	dc.w	.dir-HEADER	; ws_CurrentDir
+	dc.w	0		; ws_DontCache
+	dc.b	0		; ws_KeyDebug
+	dc.b	QUITKEY		; ws_KeyExit
+	dc.l	0		; ws_ExpMem
+	dc.w	.name-HEADER	; ws_name
+	dc.w	.copy-HEADER	; ws_copy
+	dc.w	.info-HEADER	; ws_info
+
+; v16
+	dc.w	0		; ws_kickname
+	dc.l	0		; ws_kicksize
+	dc.w	0		; ws_kickcrc
+
+; v17
+	dc.w	.config-HEADER	; ws_config
+
+
+.config	dc.b	0
+
+.dir	IFD	DEBUG
+	dc.b	"SOURCES:WHD_Slaves/Demos/VisualArts/SoundtrackerInjury4/"
+	ENDC
+	dc.b	"data",0
+
+.name	dc.b	"Soundtracker Injury 4",0
+.copy	dc.b	"1989 Visual Arts",0
+.info	dc.b	"installed by StingRay/[S]carab^Scoopex",10
+	IFD	DEBUG
+	dc.b	"DEBUG!!! "
+	ENDC
+	dc.b	"Version 1.00 (16.05.2020)",0
+
+Name	dc.b	"injury",0
+
+	CNOP	0,4
+
+resload	dc.l	0
+
+Patch	lea	resload(pc),a1
+	move.l	a0,(a1)
+	move.l	a0,a2
+
+	; install level 2 interrupt
+	bsr	SetLev2IRQ
+
+
+	; load demo
+	lea	Name(pc),a0
+	lea	$39d00,a1
+	move.l	a1,a5
+	jsr	resload_LoadFileDecrunch(a2)
+	move.l	a5,a0
+	jsr	resload_CRC16(a2)
+	cmp.w	#$2A04,d0
+	beq.b	.ok
+
+	pea	(TDREASON_WRONGVER).w
+	bra.w	EXIT
+
+.ok
+
+	; patch
+	lea	PLMAIN(pc),a0
+	move.l	a5,a1
+	move.l	resload(pc),a2
+	jsr	resload_Patch(a2)
+
+
+	; enable DMA (copper, blitter, bitplanes, sprites)
+	move.w	#$83e0,$dff096
+
+	; and run
+	jmp	$3a000-$39d00(a5)
+
+
+
+
+QUIT	pea	(TDREASON_OK).w
+EXIT	move.l	resload(pc),a2
+	bsr.b	KillSys
+	jmp	resload_Abort(a2)
+
+
+KillSys	move.w	#$7fff,$dff09a
+	bsr.b	WaitRaster
+	move.w	#$7ff,$dff096
+	move.w	#$7fff,$dff09c
+	rts
+
+
+WaitRaster
+.wait1	btst	#0,$dff005
+	beq.b	.wait1
+.wait2	btst	#0,$dff005
+	bne.b	.wait2
+	rts
+
+
+
+; ---------------------------------------------------------------------------
+
+PLMAIN	PL_START
+	PL_PSS	$19ce,FixDMAWait,2
+	PL_P	$1b8c,FixAudXVol
+	PL_PSA	$117e,LoadTune,$118a
+	PL_PSA	$1168,LoadTune,$1174
+	PL_ORW	$1612+2,1<<9		; set Bplcon0 color bit
+	PL_ORW	$1666+2,1<<9		; set Bplcon0 color bit
+	PL_SA	$318,$31c		; don't call mt_init (no tune loaded)
+
+	PL_PS	$10ba,.GetKey
+	PL_END
+
+
+.GetKey	moveq	#0,d2
+	move.b	Key(pc),d2
+
+	rts
+
+
+LoadTune
+	move.l	$39d00+$128e,a0
+	addq.w	#4,a0			; skip "df0:"
+	lea	$51000,a1
+	move.l	resload(pc),a2
+	jmp	resload_LoadFileDecrunch(a2)
+
+FixDMAWait
+	movem.l	d0/d1,-(a7)
+	moveq	#5-1,d0
+.loop	move.b	$dff006,d1
+.wait	cmp.b	$dff006,d1
+	beq.b	.wait
+	dbf	d0,.loop
+	movem.l	(a7)+,d0/d1
+	rts
+
+FixAudXVol
+	move.l	d0,-(a7)
+	moveq	#0,d0
+	move.b	3(a6),d0
+	move.w	d0,8(a5)
+	move.l	(a7)+,d0
+	rts
+
+
+***********************************
+*** Level 2 IRQ			***
+***********************************
+
+SetLev2IRQ
+	pea	.int(pc)
+	move.l	(a7)+,$68.w
+
+	move.b	#1<<7|1<<3,$bfed01		; enable keyboard interrupts
+	tst.b	$bfed01				; clear all CIA A interrupts
+	and.b	#~(1<<6),$bfee01		; set input mode
+
+	move.w	#1<<3,$dff09c			; clear ports interrupt
+	move.w	#1<<15|1<<14|1<<3,$dff09a	; and enable it
+	rts
+
+.int	movem.l	d0-d1/a0-a2,-(a7)
+	lea	$dff000,a0
+	lea	$bfe001,a1
+
+
+	btst	#3,$1e+1(a0)			; PORTS irq?
+	beq.b	.end
+
+	btst	#3,$d00(a1)			; KBD irq?
+	beq.b	.end
+
+	lea	Key(pc),a2
+	move.b	$c00(a1),d0
+	move.b	d0,(a2)
+
+	not.b	d0
+	ror.b	d0
+
+	or.b	#1<<6,$e00(a1)			; set output mode
+
+	cmp.b	HEADER+ws_keyexit(pc),d0
+	beq.w	QUIT
+
+.nokeys	moveq	#3-1,d1
+.loop	move.b	$6(a0),d0
+.wait	cmp.b	$6(a0),d0
+	beq.b	.wait
+	dbf	d1,.loop
+
+
+	and.b	#~(1<<6),$e00(a1)	; set input mode
+.end	move.w	#1<<3,$9c(a0)
+	move.w	#1<<3,$9c(a0)		; twice to avoid a4k hw bug
+	movem.l	(a7)+,d0-d1/a0-a2
+	rte
+
+
+
+Key	dc.b	0
+	dc.b	0
