0b2f8f49997ef9a9ed2e4efa5e599a351f7fd27d
StingRay/Scarab^Scoopex <32897354+MK1Roxxor@users.noreply.github.com>
2022-04-10 16:38:18 +0200

    CD32 joypad support fixed (AGA version)

--- Games/SecondSamuraiAGA_WHD.s
+++ Games/SecondSamuraiAGA_WHD.s
@@ -21,6 +21,10 @@
 *** History			***
 ***********************************
 
+; 10-Apr-2022	- joystick button handling change broke handling of the
+;		  "emulated" keys (wrong offsets due to copy/paste from
+;		  OCS version), thanks to Pascal Demaeseneire for bug report
+
 ; 10-Apr-2022	- joystick button handling code adapted to allow easy
 ;		  button remapping
 ;		- CUSTOM5 tooltype added to select fire2/CD32 joypad blue
@@ -160,7 +164,7 @@ HEADER	SLAVE_HEADER			; ws_security + ws_ID
 	IFD	DEBUG
 	dc.b	"DEBUG!!! "
 	ENDC
-	dc.b	"Version 1.3 (10.04.2022)",0
+	dc.b	"Version 1.3A (10.04.2022)",0
 
 Highscore_Name	dc.b	"SecondSamurai.high",0
 File_Name	dc.b	"SecondSamuraiX_XX",0
@@ -967,8 +971,8 @@ Handle_CD32_Button_Press
 	; button pressed
 	movem.l	d0-a6,-(a7)
 	move.l	d3,d0
-	lea	$1500+$3792.w,a1
-	jsr	$1500+$387c.w
+	lea	$1500+$3852.w,a1
+	jsr	$1500+$393c.w
 	movem.l	(a7)+,d0-a6
 
 .check_next_button
@@ -1000,8 +1004,8 @@ Handle_CD32_Button_Release
 	; button released
 	movem.l	d0-a6,-(a7)
 	move.l	d3,d0
-	lea	$1500+$3792.w,a1
-	jsr	$1500+$389c.w
+	lea	$1500+$3852.w,a1
+	jsr	$1500+$395c.w
 	movem.l	(a7)+,d0-a6
 
 .button_is_currently_pressed
