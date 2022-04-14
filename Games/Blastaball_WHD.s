f864fa3d003b9201cf82ad20741723be48ddf4bf
StingRay/Scarab^Scoopex <32897354+MK1Roxxor@users.noreply.github.com>
2022-04-14 17:49:11 +0200

    Add files via upload

---
+++ Games/Blastaball_WHD.s
@@ -0,0 +1,332 @@
+***************************************************************************
+*             /                                                           *
+*       _____.__ _                                         .___.          *
+*      /    /_____________.  _________.__________.________ |   |________  *
+*  ___/____      /    ____|_/         |         /|        \|   ._      /  *
+*  \     \/      \    \     \    /    |    :___/¯|    \    \   |/     /   *
+*   \_____________\___/_____/___/_____|____|     |____|\_____________/    *
+*     -========================/===========|______\================-      *
+*                                                                         *
+*   .---.----(*(        BLASTABALL WHDLOAD SLAVE            )*)---.---.   *
+*   `-./                                                           \.-'   *
+*                                                                         *
+*                         (c)oded by StingRay                             *
+*                         --------------------                            *
+*                              April 2022                                 *
+*                                                                         *
+*                                                                         *
+***************************************************************************
+
+***********************************
+*** History			***
+***********************************
+
+; 14-Apr-2022	- work started
+;		- and finished a short while later
+
+
+	INCDIR	SOURCES:INCLUDE/
+	INCLUDE	WHDLoad.i
+
+
+FLAGS		= WHDLF_NoError|WHDLF_ClearMem|WHDLF_EmulTrap
+QUITKEY		= $59		; F10
+DEBUG
+
+	
+; absolute skip
+PL_SA	MACRO
+	PL_S	\1,\2-(\1)
+	ENDM
+
+; jsr+absolute skip
+PL_PSA	MACRO
+	PL_PS	\1,\2		; could use PSS here but it fills memory
+	PL_S	\1+6,\3-(\1+6)	; with NOPS so we use standard skip
+	ENDM
+
+
+HEADER	SLAVE_HEADER			; ws_security + ws_ID
+	dc.w	17			; ws_version
+	dc.w	FLAGS			; flags
+	dc.l	$80000			; ws_BaseMemSize
+	dc.l	0			; ws_ExecInstall
+	dc.w	Patch-HEADER		; ws_GameLoader
+	IFD	DEBUG
+	dc.w	.dir-HEADER		; ws_CurrentDir
+	ELSE
+	dc.w	0			; ws_CurrentDir
+	ENDC
+	dc.w	0			; ws_DontCache
+	dc.b	0			; ws_KeyDebug
+	dc.b	QUITKEY			; ws_KeyExit
+	dc.l	0			; ws_ExpMem
+	dc.w	.name-HEADER		; ws_name
+	dc.w	.copy-HEADER		; ws_copy
+	dc.w	.info-HEADER		; ws_info
+
+; v16
+	dc.w	0			; ws_kickname
+	dc.l	0			; ws_kicksize
+	dc.w	0			; ws_kickcrc
+
+; v17
+	dc.w	.config-HEADER		; ws_config
+
+
+.config	dc.b	0
+
+.dir	IFD	DEBUG
+	dc.b	"SOURCES:WHD_Slaves/Games/Blastaball",0
+	ENDC
+
+.name	dc.b	"Blastaball",0
+.copy	dc.b	"1987 Mastertronic",0
+.info	dc.b	"installed by StingRay/[S]carab^Scoopex",10
+	IFD	DEBUG
+	dc.b	"DEBUG!!! "
+	ENDC
+	dc.b	"Version 1.00 (14.04.2022)",0
+	CNOP	0,4
+
+
+resload	dc.l	0
+
+Patch	lea	resload(pc),a1
+	move.l	a0,(a1)
+	move.l	a0,a2
+
+	; Load game code
+	lea	$444e0,a0
+	move.l	#$6ca00,d0
+	move.l	#$9a00,d1
+	moveq	#1,d2
+	move.l	d1,d5
+	move.l	a0,a5
+	jsr	resload_DiskLoad(a2)
+
+	; Version check
+	move.l	a5,a0
+	move.l	d5,d0
+	jsr	resload_CRC16(a2)
+	cmp.w	#$851C,d0			; SPS 1557
+	beq.b	.version_ok
+	pea	(TDREASON_WRONGVER).w
+	bra.w	EXIT
+.version_ok
+
+
+	; Patch game code
+	lea	PL_GAME(pc),a0
+	move.l	a5,a1
+	bsr	Apply_Patches
+
+	; Initialise keyboard interrupt
+	bsr	Init_Level2_Interrupt
+
+	; Initialise default level 3 interrupt
+	pea	Acknowledge_Level3_Interrupt(pc)
+	move.l	(a7)+,$6c.w
+
+	; Run the game
+	jmp	(a5)
+
+
+
+; ---------------------------------------------------------------------------
+; Game patches
+
+PL_GAME	PL_START
+	PL_P	$182,.Load
+	PL_R	$c4				; disable drive motor on
+	PL_R	$e2				; disable drive motor off
+	PL_ORW	$694+2,1<<9			; set Bplcon0 color bit
+	PL_ORW	$68e+2,1<<3			; enable level 2 interrupts
+	PL_ORW	$b6+2,1<<3			; enable level 2 interrupts
+
+	; Fix loop counter for "Copy Copperlist" routine.
+	; Original routine fails to copy the complete copperlist.
+	PL_AW	$2f58+2,1			; fix loop counter
+	PL_END
+
+
+; a0.l: destination
+; d7.l: length in bytes
+; d6.l: offset
+.Load	move.l	d6,d0
+	move.l	d7,d1
+	moveq	#1,d2				; disk number
+	move.l	resload(pc),a1
+	jmp	resload_DiskLoad(a1)
+
+
+
+
+; ---------------------------------------------------------------------------
+
+Acknowledge_Level3_Interrupt
+	move.w	#1<<4|1<<5|1<<6,$dff09c
+	move.w	#1<<4|1<<5|1<<6,$dff09c
+	rte
+
+; ---------------------------------------------------------------------------
+; Support/helper routines.
+
+
+QUIT	pea	(TDREASON_OK).w
+EXIT	move.l	resload(pc),a2
+	move.l	resload(pc),-(a7)
+	addq.l	#resload_Abort,(a7)
+
+
+KillSys	move.w	#$7fff,$dff09a
+	bsr.b	WaitRaster
+	move.w	#$7ff,$dff096
+	move.w	#$7fff,$dff09c
+	rts
+
+
+WaitRaster
+.wait1	btst	#0,$dff005
+	beq.b	.wait1
+.wait2	btst	#0,$dff005
+	bne.b	.wait2
+	rts
+
+
+Fix_DMA_Wait
+	movem.l	d0/d1,-(a7)
+	moveq	#3-1,d0
+.loop	move.b	$dff006,d1
+.still_same_raster_line
+	cmp.b	$dff006,d1
+	beq.b	.still_same_raster_line
+	dbf	d0,.loop
+	movem.l	(a7)+,d0/d1
+	rts
+
+
+; ---------------------------------------------------------------------------
+
+; a0.l: file name
+; a1.l: destination
+; -----
+; d0.l: file size
+
+Load_File
+	movem.l	d1-a6,-(a7)
+	move.l	resload(pc),a2
+	jsr	resload_LoadFileDecrunch(a2)
+	movem.l	(a7)+,d1-a6
+	rts
+
+; a0.l: file name
+; ----
+; d0.l : result (0 = file does not exist)
+File_Exists
+	movem.l	d1-a6,-(a7)
+	move.l	resload(pc),a2
+	jsr	resload_GetFileSize(a2)
+	movem.l	(a7)+,d1-a6
+	rts	
+
+; a0.l: file name
+; a1.l: file data
+; d0.l: file size
+
+Save_File
+	movem.l	d0-a6,-(a7)
+	move.l	resload(pc),a2
+	jsr	resload_SaveFile(a2)
+	movem.l	(a7)+,d0-a6
+	rts
+
+; ---------------------------------------------------------------------------
+
+; a0.l: patch list
+; a1.l: destination
+
+Apply_Patches
+	movem.l	d0-a6,-(a7)
+	move.l	resload(pc),a2
+	jsr	resload_Patch(a2)
+	movem.l	(a7)+,d0-a6
+	rts
+
+
+; ---------------------------------------------------------------------------
+
+
+; d0.b: rawkey code
+
+Check_Quit_Key
+	cmp.b	HEADER+ws_keyexit(pc),d0
+	beq.w	QUIT
+	rts	
+
+
+; Level 2 interrupt used to handle keyboard events.
+
+Init_Level2_Interrupt
+	pea	.int(pc)
+	move.l	(a7)+,$68.w
+
+	move.b	#1<<7|1<<3,$bfed01		; enable keyboard interrupts
+	tst.b	$bfed01				; clear all CIA A interrupts
+	and.b	#~(1<<6),$bfee01		; set input mode
+
+	move.w	#1<<3,$dff09c			; clear ports interrupt
+	move.w	#1<<15|1<<14|1<<3,$dff09a	; and enable it
+	rts
+
+.int	movem.l	d0-d1/a0-a2,-(a7)
+	lea	$dff000,a0
+	lea	$bfe001,a1
+
+	btst	#3,$1e+1(a0)			; PORTS irq?
+	beq.b	.end
+
+	btst	#3,$d00(a1)			; KBD irq?
+	beq.b	.end
+
+	lea	Key(pc),a2
+	moveq	#0,d0
+	move.b	$bfec01-$bfe001(a1),d0
+	move.b	d0,(a2)+
+	not.b	d0
+	ror.b	d0
+	move.b	d0,(a2)+
+	or.b	#1<<6,$e00(a1)			; set output mode
+
+	bsr.b	Check_Quit_Key
+
+	move.l	(a2)+,d1
+	beq.b	.nocustom
+	movem.l	d0-a6,-(a7)
+	move.l	d1,a0
+	jsr	(a0)
+	movem.l	(a7)+,d0-a6
+.nocustom
+	
+
+.nokeys	moveq	#3-1,d1
+.loop	move.b	$dff006-$dff000(a0),d0
+.wait	cmp.b	$dff006-$dff000(a0),d0
+	beq.b	.wait
+	dbf	d1,.loop
+
+
+	and.b	#~(1<<6),$e00(a1)	; set input mode
+.end	move.w	#1<<3,$9c(a0)
+	move.w	#1<<3,$9c(a0)		; twice to avoid a4k hw bug
+	movem.l	(a7)+,d0-d1/a0-a2
+	rte
+
+
+
+Key	dc.b	0
+RawKey	dc.b	0
+KbdCust	dc.l	0			; ptr to custom routine
+
+; ---------------------------------------------------------------------------
+
