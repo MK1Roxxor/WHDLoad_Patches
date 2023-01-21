b37b64ab78982e8ab7454852b282010272a8aa3e
StingRay/Scarab^Scoopex <32897354+MK1Roxxor@users.noreply.github.com>
2023-01-21 16:32:05 +0100

    Patch for "Musical Massacre" by Cycron

---
+++ Demos/Musical_Massacre_WHD.s
@@ -0,0 +1,422 @@
+***************************************************************************
+*             /                                                           *
+*       _____.__ _                                         .___.          *
+*      /    /_____________.  _________.__________.________ |   |________  *
+*  ___/____      /    ____|_/         |         /|        \|   ._      /  *
+*  \     \/      \    \     \    /    |    :___/¯|    \    \   |/     /   *
+*   \_____________\___/_____/___/_____|____|     |____|\_____________/    *
+*     -========================/===========|______\================-      *
+*                                                                         *
+*   .---.----(*(    MUSICAL MASSACRE/CYCRON WHDLOAD SLAVE   )*)---.---.   *
+*   `-./                                                           \.-'   *
+*                                                                         *
+*                         (c)oded by StingRay                             *
+*                         --------------------                            *
+*                            January 2023                                 *
+*                                                                         *
+*                                                                         *
+***************************************************************************
+
+***********************************
+*** History			***
+***********************************
+
+; 21-Jan-2023	- work started
+;		- and finished a short while later, OS stuff disabled,
+;		  Bytekiller decruncher optimised and relocated, delay
+;		  for title picture added
+
+
+	INCDIR	SOURCES:INCLUDE/
+	INCLUDE	WHDLoad.i
+	INCLUDE	Hardware/dmabits.i
+	INCLUDE	hardware/intbits.i
+
+
+FLAGS		= WHDLF_NoError|WHDLF_ClearMem|WHDLF_EmulTrap
+QUITKEY		= $59		; F10
+;DEBUG
+
+
+
+	
+; absolute skip
+PL_SA	MACRO
+	PL_S	\1,\2-(\1)
+	ENDM
+
+; jsr+absolute skip
+PL_PSA	MACRO
+	PL_PS	\1,\2		; could use PSS here but it fills memory
+	PL_S	\1+6,\3-(\1+6)	; with NOPS so we use standard skip
+	ENDM
+
+
+HEADER	SLAVE_HEADER			; ws_security + ws_ID
+	dc.w	17			; ws_version
+	dc.w	FLAGS			; flags
+	dc.l	$80000			; ws_BaseMemSize
+	dc.l	0			; ws_ExecInstall
+	dc.w	Patch-HEADER		; ws_GameLoader
+	IFD	DEBUG
+	dc.w	.dir-HEADER		; ws_CurrentDir
+	ELSE
+	dc.w	0
+	ENDC
+	dc.w	0			; ws_DontCache
+	dc.b	0			; ws_KeyDebug
+	dc.b	QUITKEY			; ws_KeyExit
+	dc.l	0			; ws_ExpMem
+	dc.w	.name-HEADER		; ws_name
+	dc.w	.copy-HEADER		; ws_copy
+	dc.w	.info-HEADER		; ws_info
+
+; v16
+	dc.w	0			; ws_kickname
+	dc.l	0			; ws_kicksize
+	dc.w	0			; ws_kickcrc
+
+; v17
+	dc.w	.config-HEADER		; ws_config
+
+
+.config	dc.b	0
+
+.dir	IFD	DEBUG
+	dc.b	"SOURCES:WHD_Slaves/Demos/Cycron/MusicalMassacre",0
+	ENDC
+
+.name	dc.b	"Musical Massacre",0
+.copy	dc.b	"1990 Cycron",0
+.info	dc.b	"installed by StingRay/[S]carab^Scoopex",10
+	IFD	DEBUG
+	dc.b	"DEBUG (do NOT release!) "
+	ENDC
+	dc.b	"Version 1.00 (21.01.2023)",0
+
+	CNOP	0,4
+
+
+
+resload	dc.l	0
+
+Patch	lea	resload(pc),a1
+	move.l	a0,(a1)
+	move.l	a0,a2
+
+	; Initialise level 2 interrupt for 68000 quitkey support
+	bsr	Init_Level2_Interrupt
+
+	; Load demo code
+	move.l	#$d6800,d0
+	move.l	#$5800,d1
+	lea	$78000,a0
+	bsr	Disk_Load
+
+	move.l	a0,a5
+
+	; Version check
+	move.l	a5,a0
+	move.l	d1,d0
+	jsr	resload_CRC16(a2)
+	cmp.w	#$D812,d0
+	beq.b	.Version_is_Supported
+
+	pea	(TDREASON_WRONGVER).w
+	bra.w	EXIT
+
+
+.Version_is_Supported
+
+	; Patch demo
+	lea	PL_DEMO(pc),a0
+	move.l	a5,a1
+	bsr	Apply_Patches
+
+	; And run it
+	jmp	(a5)
+	
+
+
+
+; ---------------------------------------------------------------------------
+
+PL_DEMO	PL_START
+	PL_SA	$a,$32			; skip OS stuff
+	PL_P	$3186,.Load_Data
+	PL_ORW	$124+2,INTF_PORTS	; enable level 2 interrupts
+	PL_P	$33e4,ByteKiller_Decrunch
+	PL_PS	$b0,.Display_Title_Picture
+	PL_END
+
+.Display_Title_Picture
+	moveq	#5*10,d0		; display title picture for 5 seconds
+	move.l	resload(pc),a0
+	jsr	resload_Delay(a0)
+	lea	$5d000,a0
+	rts
+
+
+.Load_Data
+	mulu.w	#512*11,d0
+	mulu.w	#512*11,d1
+	bra.w	Disk_Load
+
+; ---------------------------------------------------------------------------
+; Support/helper routines.
+
+
+QUIT	pea	(TDREASON_OK).w
+EXIT	move.l	resload(pc),a2
+	move.l	resload(pc),-(a7)
+	addq.l	#resload_Abort,(a7)
+
+
+KillSys	move.w	#$7fff,$dff09a
+	bsr.b	WaitRaster
+	move.w	#$7ff,$dff096
+	move.w	#$7fff,$dff09c
+	rts
+
+
+WaitRaster
+.wait1	btst	#0,$dff005
+	beq.b	.wait1
+.wait2	btst	#0,$dff005
+	bne.b	.wait2
+	rts
+
+
+
+; d0.w: number of raster lines to wait
+Wait_Raster_Lines
+	move.w	d1,-(a7)
+.loop	move.b	$dff006,d1
+.still_in_same_raster_line
+	cmp.b	$dff006,d1
+	beq.b	.still_in_same_raster_line	
+	subq.w	#1,d0
+	bne.b	.loop
+	move.w	(a7)+,d1
+	rts
+
+; ---------------------------------------------------------------------------
+
+; d0.l: offset
+; d1.l: size
+; a0.l: destination
+
+Disk_Load
+	movem.l	d0-a6,-(a7)
+	moveq	#1,d2			; disk number
+	move.l	resload(pc),a2
+	jsr	resload_DiskLoad(a2)
+	movem.l	(a7)+,d0-a6
+	rts
+
+; ---------------------------------------------------------------------------
+
+; a0.l: patch list
+; a1.l: destination
+
+Apply_Patches
+	movem.l	d0-a6,-(a7)
+	move.l	resload(pc),a2
+	jsr	resload_Patch(a2)
+	movem.l	(a7)+,d0-a6
+	rts
+
+
+; ---------------------------------------------------------------------------
+
+; d0.b: rawkey code
+
+Check_Quit_Key
+	cmp.b	HEADER+ws_keyexit(pc),d0
+	beq.w	QUIT
+	rts	
+
+
+; Level 2 interrupt used to handle keyboard events.
+
+Init_Level2_Interrupt
+	pea	.int(pc)
+	move.l	(a7)+,$68.w
+
+	move.b	#1<<7|1<<3,$bfed01		; enable keyboard interrupts
+	tst.b	$bfed01				; clear all CIA A interrupts
+	and.b	#~(1<<6),$bfee01		; set input mode
+
+	move.w	#1<<3,$dff09c			; clear ports interrupt
+	move.w	#1<<15|1<<14|1<<3,$dff09a	; and enable it
+	rts
+
+.int	movem.l	d0-d1/a0-a2,-(a7)
+	lea	$dff000,a0
+	lea	$bfe001,a1
+
+	btst	#3,$1e+1(a0)			; PORTS irq?
+	beq.b	.end
+
+	btst	#3,$d00(a1)			; KBD irq?
+	beq.b	.end
+
+	lea	Key(pc),a2
+	moveq	#0,d0
+	move.b	$bfec01-$bfe001(a1),d0
+	move.b	d0,(a2)+
+	not.b	d0
+	ror.b	d0
+	move.b	d0,(a2)+
+	or.b	#1<<6,$e00(a1)			; set output mode
+
+	bsr.b	Check_Quit_Key
+
+	move.l	(a2)+,d1
+	beq.b	.nocustom
+	movem.l	d0-a6,-(a7)
+	move.l	d1,a0
+	jsr	(a0)
+	movem.l	(a7)+,d0-a6
+.nocustom
+	
+	moveq	#3,d0
+	bsr	Wait_Raster_Lines
+
+	and.b	#~(1<<6),$e00(a1)	; set input mode
+.end	move.w	#1<<3,$9c(a0)
+	move.w	#1<<3,$9c(a0)		; twice to avoid a4k hw bug
+	movem.l	(a7)+,d0-d1/a0-a2
+	rte
+
+
+
+Key	dc.b	0
+RawKey	dc.b	0
+KbdCust	dc.l	0			; ptr to custom routine
+
+; ---------------------------------------------------------------------------
+
+; Bytekiller decruncher
+; resourced and adapted by stingray
+
+ByteKiller_Decrunch
+	bsr.b	.decrunch
+	tst.l	d5
+	beq.b	.ok
+
+; checksum doesn't match, file corrupt
+	pea	.ErrTxt(pc)
+	pea	(TDREASON_FAILMSG).w
+	move.l	resload(pc),a0
+	jmp	resload_Abort(a0)
+
+
+.ok	rts
+
+.ErrTxt	dc.b	"Decrunching failed, file corrupt!",0
+	cnop	0,4
+
+.decrunch
+	move.l	(a0)+,d0
+	move.l	(a0)+,d1
+	move.l	(a0)+,d5
+	move.l	a1,a2
+	add.l	d0,a0
+	add.l	d1,a2
+	move.l	-(a0),d0
+	eor.l	d0,d5
+.loop	lsr.l	#1,d0
+	bne.b	.nonew1
+	bsr.b	.nextlong
+.nonew1	bcs.b	.getcmd
+
+	moveq	#8,d1
+	moveq	#1,d3
+	lsr.l	#1,d0
+	bne.b	.nonew2
+	bsr.b	.nextlong
+.nonew2	bcs.b	.copyunpacked
+
+; data is packed, unpack and copy
+	moveq	#3,d1			; next 3 bits: length of packed data
+	clr.w	d4
+
+; d1: number of bits to get from stream
+; d4: length
+.packed	bsr.b	.getbits
+	move.w	d2,d3
+	add.w	d4,d3
+.copypacked
+	moveq	#8-1,d1
+.getbyte
+	lsr.l	#1,d0
+	bne.b	.nonew3
+	bsr.b	.nextlong
+.nonew3	addx.l	d2,d2
+	dbf	d1,.getbyte
+
+	move.b	d2,-(a2)
+	dbf	d3,.copypacked
+	bra.b	.next
+
+.ispacked
+	moveq	#8,d1
+	moveq	#8,d4
+	bra.b	.packed
+
+.getcmd	moveq	#2,d1			; next 2 bits: command
+	bsr.b	.getbits
+	cmp.b	#2,d2			; %10: unpacked data follows
+	blt.b	.notpacked
+	cmp.b	#3,d2			; %11: packed data follows
+	beq.b	.ispacked
+
+; %10
+	moveq	#8,d1			; next byte:
+	bsr.b	.getbits		; length of unpacked data
+	move.w	d2,d3			; length -> d3
+	moveq	#12,d1
+	bra.b	.copyunpacked
+
+; %00 or %01
+.notpacked
+	moveq	#9,d1
+	add.w	d2,d1
+	addq.w	#2,d2
+	move.w	d2,d3
+
+.copyunpacked
+	bsr.b	.getbits		; get offset (d2)
+	subq.w	#1,d2
+.copy	move.b	(a2,d2.w),-(a2)
+	dbf	d3,.copy
+
+.next	cmp.l	a2,a1
+	blt.b	.loop
+	rts
+
+.nextlong
+	move.l	-(a0),d0
+	eor.l	d0,d5
+	move.w	#$10,ccr
+	roxr.l	#1,d0
+	rts
+
+; d1.w: number of bits to get
+; ----
+; d2.l: bit stream
+
+.getbits
+	subq.w	#1,d1
+	clr.w	d2
+.getbit	lsr.l	#1,d0
+	bne.b	.nonew
+	move.l	-(a0),d0
+	eor.l	d0,d5
+	move.w	#$10,ccr
+	roxr.l	#1,d0
+.nonew	addx.l	d2,d2
+	dbf	d1,.getbit
+	rts
+
