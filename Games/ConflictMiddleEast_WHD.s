6e60c7581302a52371c73e7ff355c9f343ab6603
StingRay/Scarab^Scoopex <32897354+MK1Roxxor@users.noreply.github.com>
2023-07-04 10:50:48 +0200

    another version supported, CRC/patch code partly rewritten

--- Games/ConflictMiddleEast_WHD.s
+++ Games/ConflictMiddleEast_WHD.s
@@ -21,13 +21,19 @@
 *** History			***
 ***********************************
 
+; 04-Jul-2023	- unused code removed
+;		- code clean-up
+
+; 03-Jul-2023	- support for another version added ("Protec" protected)
+;		- routine to detect and patch the supported versions
+;		  partly rewritten
+
 ; 14-May-2013	- work started
 
 
 	INCDIR	SOURCES:Include/
 	INCLUDE	whdload.i
 	INCLUDE	whdmacros.i
-	;INCLUDE	lvo/dos.i
 
 ; absolute skip
 PL_SA	MACRO
@@ -86,10 +92,9 @@ slv_keyexit	= $59	; F10
 
 
 slv_CurrentDir	IFD	DEBUG
-		dc.b	"SOURCES:WHD_Slaves/ConflictMiddleEast/data",0
-		ELSE
-		dc.b	"data",0
+		dc.b	"SOURCES:WHD_Slaves/ConflictMiddleEast/"
 		ENDIF
+		dc.b	"data",0
 
 slv_name	dc.b	"Conflict: The Middle East Political Simulator",0
 slv_copy	dc.b	"1990 Virgin Mastertronic",0
@@ -97,7 +102,7 @@ slv_info	dc.b	"installed by StingRay/[S]carab^Scoopex",10
 		IFD	DEBUG
 		dc.b	"Debug!!! "
 		ENDC
-		dc.b	"Version 1.00 (14.05.2013)",0
+		dc.b	"Version 1.01 (04.07.2023)",0
 slv_config	dc.b	0
 		CNOP	0,4
 
@@ -110,8 +115,6 @@ _bootdos
 	move.l	d0,a6
 
 	move.l	_resload(pc),a2
-	lea	TAGLIST(pc),a0
-	jsr	resload_Control(a2)
 
 	lea	.name(pc),a0
 	bsr.b	.do
@@ -132,65 +135,83 @@ _bootdos
 	jmp	_LVOUnLoadSeg(a6)
 
 ; a0.l: file name
-; a1.l: patch table or 0 if no patching is required
+; a1.l: patch table
+; a2.l: resload
 ; a6.l: dos base
 ; ---
 ; d7: segment
 
 .LoadAndPatch
-	move.l	a0,d6
-	move.l	a1,a5
-	move.l	a1,a4
+	move.l	a1,d6
+
+	; Load executable
 	move.l	a0,d1
+	move.l	a0,d2			; save file name for error handling
 	jsr	_LVOLoadSeg(a6)
 	move.l	d0,d7
-	beq.b	.error
+	beq.b	.File_Error
 
-	move.l	a5,d0
-	beq.b	.exit
+	; Detect version and apply patches
+	move.l	d6,a1
 
-.loop	movem.w	(a5)+,d0-d4	; checksum, start, end, hunk, patch list
-	move.w	d0,d6
-	beq.b	.found		; if checksum is 0 we don't care about it
-	
-; move to correct hunk
-	move.l	d7,a0
-	bra.b	.enter		; hunk 0 is special case
-.gethunk
-	move.l	(a0),a0
-.enter	add.l	a0,a0
-	add.l	a0,a0
-	dbf	d3,.gethunk
-
-	lea	4(a0,d1.w),a0
-	moveq	#0,d0
-	move.w	d2,d0
-	sub.w	d1,d0
-	move.l	_resload(pc),a2
+.parse_all_entries
+	move.l	PT_START_OFFSET(a1),d1
+	bsr.b	.Get_File_Data_For_CRC16
+	move.l	PT_END_OFFSET(a1),d0
+	sub.l	PT_START_OFFSET(a1),d0
+	move.l	a1,-(a7)
 	jsr	resload_CRC16(a2)
-	cmp.w	d0,d6
-	beq.b	.found
-	tst.w	(a5)
-	bne.b	.loop
+	move.l	(a7)+,a1
+	cmp.w	PT_CHECKSUM(a1),d0
+	beq.b	.Supported_Version_Found
+	add.w	#PT_ENTRY_SIZE,a1
+	tst.w	(a1)
+	bne.b	.parse_all_entries
 
-
-; unsupported/unknown version
-.wrongver
+	; No supported version found
 	pea	(TDREASON_WRONGVER).w
 	bra.b	EXIT
 
+.Supported_Version_Found
+	move.l	d6,a0
+	add.w	PT_PATCH_LIST(a1),a0
+	move.l	d7,a1
+	jmp	resload_PatchSeg(a2)
 
-.found	lea	(a4,d4.w),a0
-.patch	move.l	d7,a1
-	move.l	_resload(pc),a2
-	jsr	resload_PatchSeg(a2)
-.exit	rts
 
-.error	jsr	_LVOIoErr(a6)
-	move.l	d6,-(a7)
+; d7.l: segment
+; d1.l: offset
+; ----
+; a0.l: file data pointer for CRC16
+
+.Get_File_Data_For_CRC16
+	move.l	d7,d0
+.parse_hunks
+	lsl.l	#2,d0
+	move.l	d0,a5
+	move.l	-4(a5),d2	; hunk length
+	subq.l	#2*4,d2		; header consists of 2 longs
+	move.l	(a5),d0		; next hunk
+
+	cmp.l	d1,d2
+	bge.b	.correct_hunk_found
+
+	sub.l	d2,d1
+	bra.b	.parse_hunks
+
+
+.correct_hunk_found
+	lea	4(a5,d1.l),a0
+	rts
+
+
+; d2.l: file name
+.File_Error
+	jsr	_LVOIoErr(a6)
+	move.l	d2,-(a7)
 	move.l	d0,-(a7)
 	pea	(TDREASON_DOSREAD).w
-	bra.b	EXIT
+	bra.b	EXIT	
 
 
 .cmd	dc.b	"",13,0			; fake command line
@@ -204,28 +225,37 @@ EXIT	move.l	_resload(pc),a2
 	jmp	resload_Abort(a2)
 
 
-; format: checksum, start, end, hunk, offset to patch list
-; start/end offsets are relative to the hunk start!
+; format: checksum, start, end, offset to patch list
 PT_GAME
 
-; game
-	dc.w	$3a96			; checksum
-	dc.w	$1fc,$266		; start, end for CRC16
-	dc.w	0			; hunk
-	dc.w	PLGAME-PT_GAME
+	; V1, unprotected
+	dc.w	$2c0f			; checksum
+	dc.l	$283d0,$283f5		; start, end for CRC16
+	dc.w	PLGAME-PT_GAME		; offset to patch list
 
-	dc.w	0			; end of tab
+	; V2 (Christoph Gleisberg, "Protec" protected)
+	dc.w	$2c0f			; checksum
+	dc.l	$28698,$286bd		; start, end for CRC16
+	dc.w	PLGAME_V2-PT_GAME	; offset to patch list
+
+	dc.w	0			; end of table
+
+
+		RSRESET
+PT_CHECKSUM	rs.w	1
+PT_START_OFFSET	rs.l	1
+PT_END_OFFSET	rs.l	1
+PT_PATCH_LIST	rs.w	1		; relative to start of patch table
+PT_ENTRY_SIZE	rs.b	0
 
 
 PLGAME	PL_START
 	PL_END
 
-
-
-TAGLIST		dc.l	WHDLTAG_ATTNFLAGS_GET
-ATTNFLAGS	dc.l	0
-		dc.l	TAG_END
-
+PLGAME_V2
+	PL_START
+	PL_R	$25488			; disable proection check
+	PL_END
 
 
 
