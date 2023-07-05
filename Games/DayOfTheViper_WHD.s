ff4c2cd821d9c579e8a339bb76e86bf8c795aa9c
StingRay/Scarab^Scoopex <32897354+MK1Roxxor@users.noreply.github.com>
2023-07-05 20:44:04 +0200

    Add files via upload

---
+++ Games/DayOfTheViper_WHD.s
@@ -0,0 +1,278 @@
+***************************************************************************
+*             /                                                           *
+*       _____.__ _                                         .___.          *
+*      /    /_____________.  _________.__________.________ |   |________  *
+*  ___/____      /    ____|_/         |         /|        \|   ._      /  *
+*  \     \/      \    \     \    /    |    :___/¯|    \    \   |/     /   *
+*   \_____________\___/_____/___/_____|____|     |____|\_____________/    *
+*     -========================/===========|______\================-      *
+*                                                                         *
+*   .---.----(*(      DAY OF THE VIPER WHDLOAD SLAVE        )*)---.---.   *
+*   `-./                                                           \.-'   *
+*                                                                         *
+*                         (c)oded by StingRay                             *
+*                         --------------------                            *
+*                               May 2013                                  *
+*                                                                         *
+*                                                                         *
+***************************************************************************
+
+***********************************
+*** History			***
+***********************************
+
+; 07-May-2013	- work started
+
+
+	INCDIR	SOURCES:Include/
+	INCLUDE	whdload.i
+	INCLUDE	whdmacros.i
+	;INCLUDE	lvo/dos.i
+
+; absolute skip
+PL_SA	MACRO
+	PL_S	\1,\2-(\1)
+	ENDM
+
+MC68020	MACRO
+	ENDM
+
+;============================================================================
+
+CHIPMEMSIZE	= 524288
+FASTMEMSIZE	= 0
+NUMDRIVES	= 1
+WPDRIVES	= %0000
+
+BLACKSCREEN
+;BOOTBLOCK
+BOOTDOS
+;BOOTEARLY
+;CBDOSLOADSEG
+;CBDOSREAD
+CACHE
+;DEBUG
+;DISKSONBOOT
+;DOSASSIGN
+FONTHEIGHT	= 8
+HDINIT
+HRTMON
+;INITAGA
+;INIT_AUDIO
+;INIT_GADTOOLS
+INIT_LOWLEVEL
+;INIT_MATHFFP
+IOCACHE		= 4096
+;JOYPADEMU
+;MEMFREE	= $200
+;NEEDFPU
+NO68020
+POINTERTICKS	= 1
+;PROMOTE_DISPLAY
+STACKSIZE	= 6000
+;TRDCHANGEDISK
+
+;============================================================================
+
+slv_Version	= 17
+slv_Flags	= WHDLF_NoError|WHDLF_Examine
+slv_keyexit	= $59	; F10
+
+;============================================================================
+
+	INCLUDE	Sources:whdload/kick13.s
+
+;============================================================================
+
+
+slv_CurrentDir	IFD	DEBUG
+		dc.b	"SOURCES:WHD_Slaves/DayOfTheViper/data",0
+		ELSE
+		dc.b	"data",0
+		ENDIF
+
+slv_name	dc.b	"Day of the Viper",0
+slv_copy	dc.b	"1989 Accolade",0
+slv_info	dc.b	"installed by StingRay/[S]carab^Scoopex",10
+		IFD	DEBUG
+		dc.b	"Debug!!! "
+		ENDC
+		dc.b	"Version 1.00 (07.05.2013)",0
+slv_config	dc.b	"C1:B:Skip Intro;"
+		dc.b	"C2:B:Unlimited Energy;"
+		dc.b	0
+		CNOP	0,4
+
+	IFD BOOTDOS
+
+_bootdos
+	lea	_dosname(pc),a1
+	move.l	$4.w,a6
+	jsr	_LVOOldOpenLibrary(a6)
+	move.l	d0,a6
+
+	move.l	_resload(pc),a2
+	lea	TAGLIST(pc),a0
+	jsr	resload_Control(a2)
+
+.runagain
+	lea	.ix2(pc),a0
+	tst.l	ENERGYTRAINER-.ix2(a0)
+	bne.b	.trainerenabled
+	clr.w	PLTR-.ix2(a0)		; PLCMD_END
+
+
+.trainerenabled
+
+	tst.l	NOINTRO-.ix2(a0)
+	bne.b	.skip
+	lea	PT_IX2(pc),a1
+	bsr.b	.do2			; $56180AC2
+
+.skip	lea	.vx2(pc),a0
+	bsr.b	.do
+
+	sub.l	a1,a1
+	lea	.wx2(pc),a0		; good end
+	cmp.w	#10,d6
+	beq.b	.go
+	lea	.ex2(pc),a0		; bad end
+.go	bsr.b	.do2
+	tst.w	d6
+	bne.b	.runagain
+	bra.w	QUIT
+
+
+.do	lea	PT_GAME(pc),a1
+.do2	bsr.b	.LoadAndPatch
+.run	move.l	d7,a1
+	add.l	a1,a1
+	add.l	a1,a1
+	movem.l	d7/a6,-(a7)
+	lea	.cmd(pc),a0
+	moveq	#.cmdlen,d0
+	jsr	4(a1)
+	move.l	d0,d6
+	movem.l	(a7)+,d7/a6
+	move.l	d7,d1
+	jmp	_LVOUnLoadSeg(a6)
+
+; a0.l: file name
+; a1.l: patch table or 0 if no patching is required
+; a6.l: dos base
+; ---
+; d7: segment
+
+.LoadAndPatch
+	move.l	a0,d6
+	move.l	a1,a5
+	move.l	a1,a4
+	move.l	a0,d1
+	jsr	_LVOLoadSeg(a6)
+	move.l	d0,d7
+	beq.b	.error
+
+	move.l	a5,d0
+	beq.b	.exit
+
+.loop	movem.w	(a5)+,d0-d4	; checksum, start, end, hunk, patch list
+	move.w	d0,d6
+	beq.b	.found		; if checksum is 0 we don't care about it
+	
+; move to correct hunk
+	move.l	d7,a0
+	bra.b	.enter		; hunk 0 is special case
+.gethunk
+	move.l	(a0),a0
+.enter	add.l	a0,a0
+	add.l	a0,a0
+	dbf	d3,.gethunk
+
+	lea	4(a0,d1.w),a0
+	moveq	#0,d0
+	move.w	d2,d0
+	sub.w	d1,d0
+	move.l	_resload(pc),a2
+	jsr	resload_CRC16(a2)
+	cmp.w	d0,d6
+	beq.b	.found
+	tst.w	(a5)
+	bne.b	.loop
+
+
+; unsupported/unknown version
+.wrongver
+	pea	(TDREASON_WRONGVER).w
+	bra.b	EXIT
+
+
+.found	lea	(a4,d4.w),a0
+.patch	move.l	d7,a1
+	move.l	_resload(pc),a2
+	jsr	resload_PatchSeg(a2)
+.exit	rts
+
+.error	jsr	_LVOIoErr(a6)
+	move.l	d6,-(a7)
+	move.l	d0,-(a7)
+	pea	(TDREASON_DOSREAD).w
+	bra.b	EXIT
+
+
+.cmd	dc.b	"",13,0			; fake command line
+.cmdlen	= *-.cmd
+.ix2	dc.b	"ix2",0
+.vx2	dc.b	"vx2",0
+.ex2	dc.b	"ex2",0
+.wx2	dc.b	"wx2",0
+	CNOP	0,4
+
+
+QUIT	pea	(TDREASON_OK).w
+EXIT	move.l	_resload(pc),a2
+	jmp	resload_Abort(a2)
+
+
+; format: checksum, start, end, hunk, offset to patch list
+; start/end offsets are relative to the hunk start!
+PT_GAME
+
+; game
+	dc.w	$ef9c			; checksum
+	dc.w	$7198-$7198,$71c8-$7198	; start, end for CRC16
+	dc.w	4			; hunk
+	dc.w	PLGAME-PT_GAME		; SPS 454
+
+	dc.w	0			; end of tab
+
+
+PT_IX2	dc.w	$7a1f			; checksum
+	dc.w	$356-$234,$392-$234	; start, end for CRC16
+	dc.w	3			; hunk
+	dc.w	PLIX2-PT_IX2		; SPS 454
+
+	dc.w	0			; end of tab
+
+PLIX2	PL_START
+	PL_B	$d42,$60		; disable protection check
+	PL_END
+	
+
+PLGAME	PL_START
+	PL_B	$73de,$60		; disable protection check
+PLTR	PL_B	$10562,$60		; unlimited energy
+	PL_END
+
+
+
+
+TAGLIST		dc.l	WHDLTAG_ATTNFLAGS_GET
+		dc.l	0
+		dc.l	WHDLTAG_CUSTOM1_GET
+NOINTRO		dc.l	0
+		dc.l	WHDLTAG_CUSTOM2_GET
+ENERGYTRAINER	dc.l	0
+		dc.l	TAG_END
+
+	ENDC
+
