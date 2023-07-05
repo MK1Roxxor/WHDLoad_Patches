9c08d8aa77c4b92b8fa7fb5e71d825c8739f24ca
StingRay/Scarab^Scoopex <32897354+MK1Roxxor@users.noreply.github.com>
2023-07-05 20:24:40 +0200

    rewritten to use files instead of disk image

--- Games/WorldCup90_WHD.s
+++ Games/WorldCup90_WHD.s
@@ -21,6 +21,8 @@
 *** History			***
 ***********************************
 
+; 05-Jul-2023	- patch uses files instead of disk image now
+
 ; 02-Jul-2023	- byte write to volume register fixed (Mantis issue #5283)
 
 ; 22-Jan-2012	- work started
@@ -29,51 +31,53 @@
 	INCDIR	SOURCES:Include/
 	INCLUDE	whdload.i
 	INCLUDE	whdmacros.i
-	;INCLUDE	lvo/dos.i
 
 ; absolute skip
 PL_SA	MACRO
 	PL_S	\1,\2-(\1)
 	ENDM
 
-;---------------------------------------------------------------------------*
-
-	INCDIR	SOURCES:Include/
-	INCLUDE	whdload.i
-	INCLUDE	whdmacros.i
+MC68020	MACRO
+	ENDM
 
 ;============================================================================
 
-CHIPMEMSIZE	= $80000
+CHIPMEMSIZE	= 524288
 FASTMEMSIZE	= 0
 NUMDRIVES	= 1
-WPDRIVES	= %0000
+WPDRIVES	= %0001
 
 BLACKSCREEN
 ;BOOTBLOCK
-;BOOTDOS
+BOOTDOS
 ;BOOTEARLY
-CBDOSLOADSEG
+;CBDOSLOADSEG
 ;CBDOSREAD
-;CBKEYBOARD
-CACHE
+;CACHE
 ;DEBUG
-DISKSONBOOT
+;DISKSONBOOT
 ;DOSASSIGN
-FONTHEIGHT     = 8
-;HDINIT
+FONTHEIGHT	= 8
+HDINIT
 HRTMON
+;INITAGA
+;INIT_AUDIO
+;INIT_GADTOOLS
+INIT_LOWLEVEL
+;INIT_MATHFFP
 IOCACHE		= 1024
+;JOYPADEMU
 ;MEMFREE	= $200
 ;NEEDFPU
-POINTERTICKS   = 1
-SETPATCH
-;STACKSIZE	= 6000
+NO68020
+POINTERTICKS	= 1
+;PROMOTE_DISPLAY
+STACKSIZE	= 10000
 ;TRDCHANGEDISK
 
 ;============================================================================
 
-slv_Version	= 16
+slv_Version	= 17
 slv_Flags	= WHDLF_NoError|WHDLF_Examine
 slv_keyexit	= $59	; F10
 
@@ -85,97 +89,158 @@ slv_keyexit	= $59	; F10
 
 
 slv_CurrentDir	IFD	DEBUG
-		dc.b	"SOURCES:WHD_Slaves/WorldCup90/",0
-		ELSE
-		dc.b	0
-		ENDC
+		dc.b	"SOURCES:WHD_Slaves/Games/WorldCup90/"
+		ENDIF
+		dc.b	"data",0
+
 slv_name	dc.b	"World Cup 90",0
-slv_copy	dc.b	"1990 Genias.",0
+slv_copy	dc.b	"1990 Genias",0
 slv_info	dc.b	"installed by StingRay/[S]carab^Scoopex",10
-		dc.b	"Version 1.1 (02.07.2023)",0
+		IFD	DEBUG
+		dc.b	"Debug!!! "
+		ENDC
+		dc.b	"Version 2.00 (05.07.2023)",0
+slv_config	dc.b	0
 		CNOP	0,4
 
-;============================================================================
-; entry before any diskaccess is performed, no dos.library available
+	IFD BOOTDOS
 
-	IFD BOOTEARLY
+_bootdos
+	lea	_dosname(pc),a1
+	move.l	$4.w,a6
+	jsr	_LVOOldOpenLibrary(a6)
+	move.l	d0,a6
 
-_bootearly	blitz
-		rts
-
-	ENDC
-
-;============================================================================
-; bootblock from "Disk.1" has been loaded, no dos.library available
-
-	IFD BOOTBLOCK
-
-; A1 = ioreq ($2c+a5)
-; A4 = buffer (1024 bytes)
-; A6 = execbase
-
-_bootblock	blitz
-		jmp	(12,a4)
-
-	ENDC
-
-
-;============================================================================
-; callback/hook which gets executed after each successful call to dos.LoadSeg
-; can also be used instead of _bootdos
-; if you use diskimages that is the way to patch the executables
-
-; the following example uses a parameter table to patch different executables
-; after they get loaded
-
-	IFD CBDOSLOADSEG
-
-; D0 = BSTR name of the loaded program as BCPL string
-; D1 = BPTR segment list of the loaded program as BCPL pointer
-
-_cb_dosLoadSeg
-	lsl.l	#2,d0		; convert to bptr
-	move.l	d0,a0
-	move.b	(a0)+,d0	; length of name
-	lea	.name(pc),a1
-	bsr.b	.compare
-	bne.b	.nope
-
-	lsl.l	#2,d1
-	move.l	d1,a5
-	lea	$10+4(a5),a0	; +4 -> skip segment ptr
-	move.w	#$1ac-$10,d0
 	move.l	_resload(pc),a2
+
+	lea	.name(pc),a0
+	bsr.b	.do
+	bra.w	QUIT
+
+
+.do	lea	PT_GAME(pc),a1
+	bsr.b	.LoadAndPatch
+.run	move.l	d7,a1
+	add.l	a1,a1
+	add.l	a1,a1
+	movem.l	d7/a6,-(a7)
+	lea	.cmd(pc),a0
+	moveq	#.cmdlen,d0
+	jsr	4(a1)
+	movem.l	(a7)+,d7/a6
+	move.l	d7,d1
+	jmp	_LVOUnLoadSeg(a6)
+
+; a0.l: file name
+; a1.l: patch table
+; a2.l: resload
+; a6.l: dos base
+; ---
+; d7: segment
+
+.LoadAndPatch
+	move.l	a1,d6
+
+	; Load executable
+	move.l	a0,d1
+	move.l	a0,d2			; save file name for error handling
+	jsr	_LVOLoadSeg(a6)
+	move.l	d0,d7
+	beq.b	.File_Error
+
+	; Detect version and apply patches
+	move.l	d6,a1
+
+.parse_all_entries
+	move.l	PT_START_OFFSET(a1),d1
+	bsr.b	.Get_File_Data_For_CRC16
+	move.l	PT_END_OFFSET(a1),d0
+	sub.l	PT_START_OFFSET(a1),d0
+	move.l	a1,-(a7)
 	jsr	resload_CRC16(a2)
-	cmp.w	#$dd5c,d0
-	beq.b	.ok
+	move.l	(a7)+,a1
+	cmp.w	PT_CHECKSUM(a1),d0
+	beq.b	.Supported_Version_Found
+	add.w	#PT_ENTRY_SIZE,a1
+	tst.w	(a1)
+	bne.b	.parse_all_entries
+
+	; No supported version found
 	pea	(TDREASON_WRONGVER).w
+	bra.b	EXIT
+
+.Supported_Version_Found
+	move.l	d6,a0
+	add.w	PT_PATCH_LIST(a1),a0
+	move.l	d7,a1
+	jmp	resload_PatchSeg(a2)
+
+
+; d7.l: segment
+; d1.l: offset
+; ----
+; a0.l: file data pointer for CRC16
+
+.Get_File_Data_For_CRC16
+	move.l	d7,d0
+.parse_hunks
+	lsl.l	#2,d0
+	move.l	d0,a5
+	move.l	-4(a5),d2	; hunk length
+	subq.l	#2*4,d2		; header consists of 2 longs
+	move.l	(a5),d0		; next hunk
+
+	cmp.l	d1,d2
+	bge.b	.correct_hunk_found
+
+	sub.l	d2,d1
+	bra.b	.parse_hunks
+
+
+.correct_hunk_found
+	lea	4(a5,d1.l),a0
+	rts
+
+
+; d2.l: file name
+.File_Error
+	jsr	_LVOIoErr(a6)
+	move.l	d2,-(a7)
+	move.l	d0,-(a7)
+	pea	(TDREASON_DOSREAD).w
+	bra.b	EXIT	
+
+
+.cmd	dc.b	"",13,0			; fake command line
+.cmdlen	= *-.cmd
+.name	dc.b	"a",0
+	CNOP	0,4
+
+
+QUIT	pea	(TDREASON_OK).w
+EXIT	move.l	_resload(pc),a2
 	jmp	resload_Abort(a2)
 
 
-.ok	lea	PLGAME(pc),a0
-	move.l	a5,a1
-	move.l	_resload(pc),a2
-	jmp	resload_Patch(a2)
+; format: checksum, start, end, offset to patch list
+PT_GAME	dc.w	$afa4			; checksum
+	dc.l	$d5dc,$d6a0		; start, end for CRC16
+	dc.w	PLGAME-PT_GAME		; offset to patch list
+	dc.w	0			; end of table
 
 
-.compare
-.loop	move.b	(a0)+,d2
-	or.b	#1<<5,d2		; convert to lower case
-	cmp.b	(a1)+,d2
-	bne.b	.nope
-	subq.b	#1,d0
-	bne.b	.loop
-.nope	rts
+		RSRESET
+PT_CHECKSUM	rs.w	1
+PT_START_OFFSET	rs.l	1
+PT_END_OFFSET	rs.l	1
+PT_PATCH_LIST	rs.w	1		; relative to start of patch table
+PT_ENTRY_SIZE	rs.b	0
 
-
-.name	dc.b	"a",0			; lower case!
 
 PLGAME	PL_START
 	PL_PSS	$bce4,FixDMAWait,2	; fix DMA wait in replayer
 	PL_P	$bea2,Fix_Volume_Write	; fix byte write to volume register
 	PL_END
-
 
 Fix_Volume_Write
 	move.l	d0,-(a7)
@@ -195,9 +260,5 @@ FixDMAWait
 	move.l	(a7)+,d1
 	rts
 
-
-
 	ENDC
-
-
 
