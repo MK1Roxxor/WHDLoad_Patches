579a1974f3828c515d2b3a18f0ac1995a6a664d6
StingRay/Scarab^Scoopex <32897354+MK1Roxxor@users.noreply.github.com>
2023-08-14 22:22:22 +0200

    Gettysburg Patch

---
+++ Games/Gettysburg_WHD.s
@@ -0,0 +1,293 @@
+***************************************************************************
+*             /                                                           *
+*       _____.__ _                                         .___.          *
+*      /    /_____________.  _________.__________.________ |   |________  *
+*  ___/____      /    ____|_/         |         /|        \|   ._      /  *
+*  \     \/      \    \     \    /    |    :___/¯|    \    \   |/     /   *
+*   \_____________\___/_____/___/_____|____|     |____|\_____________/    *
+*     -========================/===========|______\================-      *
+*                                                                         *
+*   .---.----(*(        GETTYSBURG WHDLOAD SLAVE           )*)---.---.    *
+*   `-./                                                          \.-'    *
+*                                                                         *
+*                         (c)oded by StingRay                             *
+*                         --------------------                            *
+*                              March 2013                                 *
+*                                                                         *
+*                                                                         *
+***************************************************************************
+
+***********************************
+*** History			***
+***********************************
+
+; 24-Mar-2013	- work started
+;		- and finished some minutes later, not much to do here
+
+
+	INCDIR	SOURCES:Include/
+	INCLUDE	whdload.i
+	INCLUDE	whdmacros.i
+	;INCLUDE	lvo/dos.i
+
+; absolute skip
+PL_SA	MACRO
+	PL_S	\1,\2-(\1)
+	ENDM
+
+MC68020	MACRO
+	ENDM
+
+;============================================================================
+
+CHIPMEMSIZE	= 524288
+FASTMEMSIZE	= 0
+NUMDRIVES	= 1
+WPDRIVES	= %0000
+
+BLACKSCREEN
+;BOOTBLOCK
+BOOTDOS
+;BOOTEARLY
+;CBDOSLOADSEG
+;CBDOSREAD
+;CACHE
+;DEBUG
+;DISKSONBOOT
+;DOSASSIGN
+FONTHEIGHT	= 8
+HDINIT
+;HRTMON
+;INITAGA
+;INIT_AUDIO
+;INIT_GADTOOLS
+;INIT_LOWLEVEL
+;INIT_MATHFFP
+IOCACHE		= 1024
+;JOYPADEMU
+;MEMFREE	= $200
+;NEEDFPU
+;NO68020
+POINTERTICKS	= 1
+;PROMOTE_DISPLAY
+;STACKSIZE	= 16384
+;TRDCHANGEDISK
+
+;============================================================================
+
+slv_Version	= 17
+slv_Flags	= WHDLF_NoError|WHDLF_Examine
+slv_keyexit	= $59	; F10
+
+;============================================================================
+
+	INCLUDE	Sources:whdload/kick13.s
+
+;============================================================================
+
+
+slv_CurrentDir	IFD	DEBUG
+		dc.b	"SOURCES:WHD_Slaves/Gettysburg/data",0
+		ELSE
+		dc.b	"data",0
+		ENDIF
+
+slv_name	dc.b	"Gettysburg",0
+slv_copy	dc.b	"1990 ARC",0
+slv_info	dc.b	"installed by StingRay/[S]carab^Scoopex",10
+		IFD	DEBUG
+		dc.b	"DEBUG!!! "
+		ENDC
+		dc.b	"Version 1.00 (24.03.2013)",0
+		CNOP	0,4
+
+slv_config	dc.b	0
+		CNOP	0,4
+
+DOSbase	dc.l	0
+
+	IFD BOOTDOS
+
+_bootdos
+	lea	.saveregs(pc),a0
+	movem.l	d1-d3/d5-d7/a1-a2/a4-a6,(a0)
+
+	lea	_dosname(pc),a1
+	move.l	$4.w,a6
+	jsr	_LVOOldOpenLibrary(a6)
+	move.l	d0,a6
+	lea	DOSbase(pc),a0
+	move.l	d0,(a0)
+
+	move.l	_resload(pc),a2
+	lea	TAGLIST(pc),a0
+	jsr	resload_Control(a2)
+
+
+	
+; load game
+.dogame	move.l	#$360,d0
+	move.l	#$464-$360,d1
+	lea	.game(pc),a0
+	lea	PT_GAME(pc),a1
+.go	bsr.b	.LoadAndPatch
+
+.run	move.l	d7,d1
+	moveq	#.arglen,d0
+	lea	.args(pc),a0
+	bsr.b	.call
+	bra.w	QUIT
+
+
+
+
+; d0.l: argument length
+; d1.l: segment
+; a0.l: argument (command line)
+; a6.l: dosbase
+.call	lea	.callregs(pc),a1
+	movem.l	d2-d7/a2-a6,(a1)
+	move.l	(a7)+,11*4(a1)
+	move.l	d0,d4
+	lsl.l	#2,d1
+	move.l	d1,a3
+	move.l	a0,a4		; save command line
+; create longword aligned copy of args
+	lea	.callargs(pc),a1
+	move.l	a1,d2
+.copy	move.b	(a0)+,(a1)+
+	subq.w	#1,d0
+	bne.b	.copy
+
+; set args
+	jsr	_LVOInput(a6)
+	lsl.l	#2,d0		; BPTR -> APTR
+	move.l	d0,a0
+	lsr.l	#2,d2		; APTR -> BPTR
+	move.l	d2,fh_Buf(a0)
+	clr.l	fh_Pos(a0)
+	move.l	d4,fh_End(a0)
+; call
+	move.l	d4,d0
+	move.l	a4,a0
+
+
+	movem.l	.saveregs(pc),d1-d3/d5-d7/a1-a2/a4-a6
+
+	jsr	4(a3)
+
+
+; return
+	movem.l	.callregs(pc),d2-d7/a2-a6
+	move.l	.callrts(pc),a0
+	jmp	(a0)
+
+
+
+
+
+; d0.l: start offset for version check
+; d1.l: length for version check
+; a0.l: file name
+; a1.l: patch table
+; a6.l: dos base
+; ---
+; d7: segment
+
+.LoadAndPatch
+	move.l	d0,d3
+	move.l	d1,d4
+
+	move.l	a0,d6
+	move.l	a1,a5
+	move.l	a0,d1
+	jsr	_LVOLoadSeg(a6)
+	move.l	d0,d7
+	beq.b	.error
+
+	move.l	d7,a0
+	add.l	a0,a0
+	add.l	a0,a0
+	lea	4(a0,d3.l),a0
+	move.l	d4,d0
+	move.l	_resload(pc),a2
+	jsr	resload_CRC16(a2)
+	move.w	d0,d2
+
+; d0: checksum
+	move.l	a5,a0
+.find	move.w	(a0)+,d0
+	cmp.w	#-1,d0
+	beq.b	.out
+	tst.w	d0
+	beq.b	.wrongver
+	cmp.w	d0,d2
+	beq.b	.found
+	addq.w	#2,a0
+	bra.b	.find
+
+; unsupported/unknown version
+.wrongver
+	pea	(TDREASON_WRONGVER).w
+	bra.w	EXIT
+
+
+.found	add.w	(a0),a5
+
+
+
+	move.l	a5,a0
+.patch	move.l	d7,a1
+	move.l	_resload(pc),a2
+	jsr	resload_PatchSeg(a2)
+.out	rts
+
+.error	jsr	_LVOIoErr(a6)
+	move.l	d6,-(a7)
+	move.l	d0,-(a7)
+	pea	(TDREASON_DOSREAD).w
+	bra.w	EXIT
+
+
+.game	dc.b	"gettysburg",0
+.args	dc.b	"",10		; must be LF terminated
+.arglen	= *-.args
+	CNOP	0,4
+
+.saveregs	ds.l	11
+.saverts	dc.l	0
+.dosbase	dc.l	0
+.callregs	ds.l	11
+.callrts	dc.l	0
+.callargs	ds.b	208
+
+
+QUIT	pea	(TDREASON_OK).w
+EXIT	move.l	_resload(pc),a2
+	jmp	resload_Abort(a2)
+
+
+	CNOP 0,4
+
+
+; format: checksum, offset to patch list
+PT_GAME	dc.w	$c7bb,PLGAME-PT_GAME	; SPS 1937
+	dc.w	0			; end of tab
+
+
+PLGAME	PL_START
+	PL_PS	$347fc,.stack
+	PL_END
+
+.stack	sub.l	#6000,d0
+	addq.l	#8,d0
+	rts
+
+
+
+
+TAGLIST	dc.l	TAG_END
+
+
+
+
